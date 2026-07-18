11be8cf0d9fe858a1c320e4741050ba82f8690a6
parthsamani <parthsamani@zohomail.in>
2026-07-18 10:27:20 +0530

    Add configuration for bot token and chat ID

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+TIMEZONE = "Asia/Kolkata"
+SEND_TIME = "09:15"
+
+SYMBOLS = {
+    "NIFTY 50": "^NSEI",
+    "BANKNIFTY": "^NSEBANK",
+    "SENSEX": "^BSESN"
+}
+
+FOOTER = "© ParthTraderAlerts"
